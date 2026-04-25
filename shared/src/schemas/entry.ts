84fff5618c697d802488ef88886b31843a7702ac
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-04-25 23:29:40 +0530

    feat(shared): add full link entry schema

---
+++ shared/src/schemas/entry.ts
@@ -0,0 +1,32 @@
+import * as z from "zod";
+
+const LinkEntrySchema = z.object({
+  id: z.string(),
+  userId: z.string(),
+  type: z.literal("link"),
+  title: z
+    .string()
+    .trim()
+    .min(1, { error: "Title must contain at least 1 character" }),
+  tags: z
+    .array(
+      z
+        .string()
+        .trim()
+        .min(1, { error: "Tag name must contain at least 1 character" }),
+    )
+    .default([]),
+  data: z.object({
+    url: z.url({
+      protocol: /^https?$/,
+      hostname: z.regexes.domain,
+    }),
+    description: z
+      .string()
+      .trim()
+      .min(1, { error: "Description must contain at least 1 character" })
+      .optional(),
+  }),
+  createdAt: z.iso.datetime(),
+  updatedAt: z.iso.datetime(),
+});
