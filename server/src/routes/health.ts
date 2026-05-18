0da7f2a68a65de7f1ae0fab0424d6d6cbea2ec2e
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-05-18 23:56:53 +0530

    feat(server): add status code to response object

--- server/src/routes/health.ts
+++ server/src/routes/health.ts
@@ -1,6 +1,6 @@
 import express from "express";
 export const healthRouter = express.Router();
 
-healthRouter.get("/", (req, res) => {
-  res.json({ message: "Health OK" });
+healthRouter.get("/", (_req, res) => {
+  res.status(200).json({ message: "Health OK" });
 });
