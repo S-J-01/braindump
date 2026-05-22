b43f72aeeb625db72e36316d31a95539a5079bec
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-05-22 17:21:28 +0530

    feat(server): add logger setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,11 @@ import cors from "cors";
 import { apiRouter } from "./routes/index";
 import { notFound } from "./middleware/notFound";
 import { errorHandler } from "./middleware/errorHandler";
+import { httpLogger } from "./lib/logger";
 export const app = express();
 
 app.use(cors());
+app.use(httpLogger);
 app.use(express.json());
 app.use("/api/v1", apiRouter);
 app.use(notFound);
