a9441169b47c13a047c3f4fb469ca6a1355d33a9
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-06-22 23:57:38 +0530

    chore(server): wire cookie-parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,11 +4,13 @@ import { apiRouter } from "./routes/index";
 import { notFound } from "./middleware/notFound";
 import { errorHandler } from "./middleware/errorHandler";
 import { httpLogger } from "./lib/logger";
+import cookieParser from "cookie-parser";
 export const app = express();
 
 app.use(cors());
 app.use(httpLogger);
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1", apiRouter);
 app.use(notFound);
 app.use(errorHandler);
