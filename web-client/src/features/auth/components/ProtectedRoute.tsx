635fcdaa3b195a54113316fdd42c2e7c2b432ef9
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-08-06 23:24:06 +0530

    feat(web-client): add protected route guard

---
+++ web-client/src/features/auth/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router";
+import { useCurrentUser } from "../queries";
+
+export const ProtectedRoute = () => {
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (data === null) {
+    return <Navigate replace to="/login" />;
+  }
+  return <Outlet />;
+};
