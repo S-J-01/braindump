2521318921c0a72e16e36b28f6d0e35babef4655
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-09-03 19:28:10 +0530

    feat(web-client): add signup and login pages

--- web-client/src/routes/login.tsx
+++ web-client/src/routes/login.tsx
@@ -1,3 +1,37 @@
+import { useState, type SubmitEventHandler } from "react";
+import { AuthCard } from "../features/auth/components/AuthCard";
+import { useLogin } from "../features/auth/mutations";
+
 export const Login = () => {
-  return <>This is login</>;
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const loginMutation = useLogin();
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    loginMutation.mutate({ email, password });
+  };
+
+  return (
+    <AuthCard title="Log In" subtitle="Log In To Continue">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" disabled={loginMutation.isPending}>
+          {loginMutation.isPending ? "Logging In ..." : "Log In"}
+        </button>
+        {loginMutation.isError ? <p>Log In Failed</p> : null}
+      </form>
+    </AuthCard>
+  );
 };
--- web-client/src/routes/signup.tsx
+++ web-client/src/routes/signup.tsx
@@ -1,3 +1,37 @@
+import { useState, type SubmitEventHandler } from "react";
+import { AuthCard } from "../features/auth/components/AuthCard";
+import { useSignup } from "../features/auth/mutations";
+
 export const Signup = () => {
-  return <>This is signup</>;
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const signupMutation = useSignup();
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    signupMutation.mutate({ email, password });
+  };
+
+  return (
+    <AuthCard title="Sign Up" subtitle="Sign Up To Continue">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" disabled={signupMutation.isPending}>
+          {signupMutation.isPending ? "Signing Up ..." : "Sign Up"}
+        </button>
+        {signupMutation.isError ? <p>Sign Up Failed</p> : null}
+      </form>
+    </AuthCard>
+  );
 };
